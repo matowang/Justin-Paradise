f87be5340a66b074d00762ae0eb6dff3709cbe26
Matthew Wang <wmatthew123@gmail.com>
2019-01-15 02:21:08 +0800

    Use hash router instead of browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './components/ScrollToTop';
@@ -9,13 +9,13 @@ import { Provider } from 'react-redux';
 import store from './store.js';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <ScrollToTop>
             <Provider store={store}>
                 <App />
             </Provider>
         </ScrollToTop>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
