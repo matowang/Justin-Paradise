7ef139834f80534f037f6b98d9c0611f95b48366
Matthew Wang <wmatthew123@gmail.com>
2019-01-25 19:58:03 +0800

    dramatized landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,7 +2,7 @@ import React from "react";
 const LandingPage = (props) => {
     return (
         <div id="landing-page">
-            <div>
+            <div >
                 <p>Hi, I'm </p><h1>Justin Hoong</h1><span>,</span><p> and welcome<br /> to my<br /> paradise</p>
             </div>
             <button id="check-me-out-button">Check me out</button>
