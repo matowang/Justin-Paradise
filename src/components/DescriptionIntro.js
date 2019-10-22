99c5b95920f5c63031019d31a1b7b36e0acac8ce
Matthew Wang <wmatthew123@gmail.com>
2019-10-22 20:08:23 +0800

    justin articles

--- src/components/DescriptionIntro.js
+++ src/components/DescriptionIntro.js
@@ -5,10 +5,12 @@ class DescriptionIntro extends React.Component {
         return (
             <section id="description-intro">
                 <div className="decor-line" />
-                <h2>Lorem ipsum</h2>
-                <p>Deserunt non aliquip cillum do sit anim. Sit quis do velit consectetur velit id nostrud adipisicing. Do amet incididunt sit veniam anim id non elit reprehenderit ut laborum excepteur.Proident est ad quis duis magna Lorem irure. Elit anim irure proident minim do sit duis id aliqua tempor exercitation cupidatat sit. Id velit ullamco laborum excepteur veniam nulla nulla. Non laborum ex velit ea ea. Nostrud do do labore aute aliquip sint duis id ex in. Duis dolore dolor tempor ad consequat adipisicing quis ut. Labore dolore ea ex ea anim reprehenderit incididunt.</p>
-                <p>LoremUllamco eiusmod occaecat culpa eiusmod irure minim consequat ex id elit minim. Adipisicing nisi consequat ipsum minim officia eu Lorem consectetur. Sit magna quis magna cillum. Occaecat laborum qui nostrud exercitation magna cupidatat. Elit nisi reprehenderit proident veniam elit pariatur. Non ullamco culpa irure nostrud aliquip amet aliqua in. Ut nisi labore aliqua reprehenderit elit est id.</p>
-                <p>Qui non officia non proident eu. Nisi quis cillum veniam culpa adipisicing incididunt ullamco ipsum amet elit dolore duis. Cupidatat sunt voluptate ex commodo commodo exercitation ad laborum ut. Aliqua in elit mollit esse fugiat enim. Labore occaecat irure dolore do velit aliqua est culpa.</p>
+                <h2>About</h2>
+                <p>I’m never a traveler. But I enjoy travelling, whether it’s a one-day trip, a one-week visit, or a one-month travel. I regard studying abroad as a college student as a travel, only that the schedule of this longer trip is a bit different. </p>
+
+                <p>I feel fortunate and blissful to have this opportunity, an accomplished goal to explore the land I’ve always dreamed of. I still remember the time when I was little, holding a guidebook about San Francisco, as I read through the last page of the book that quoted, “the purpose of every trip is to plan the next trip.” The purpose of this trip, studying at college, is indeed to plan the future trips, yet this trip brings me something very unique. It’s the chance to encounter the stories of many things and many people with diverse backgrounds. The encounters of those stories have shaped me to choose nutritional science as the focus of my studies. I believe that the journey of learning nutritional science not just reflects on my interest in food and health in general, but opens up my career goal in making a community-based impact that would reach beyond the field of nutrition. I hope that this website can serve as both a platform about me and a place where I can share the stories of the many stories I’ve experienced while studying that I’d like to highlight. </p>
+
+                <p>我從來就算不上是個旅行家，但我是個喜歡旅行的人，不論是一天的旅行、一個禮拜的探訪、或是一個月的深度旅遊。我認為出國留學就是一次長期的旅行，只是這個旅行的行程比較不一樣。而我很幸運有這個機會，一個奢華又幸福的夢想實現。我還記得當我還是個小男孩的時候，看到一本介紹舊金山的旅遊手冊，手冊中最後一頁寫道: 「每一次的旅行，都是為了準備下一次的旅程。」至今令我印象深刻。留學也是為了準備未來的旅程，過程中我遇到了許許多多事情和不同背景的人的故事，這些機遇也引領我在大學選擇就讀營養學。我相信學習營養學不只滿足我對於食物和健康的興趣，而更帶領我想要為社會做出超越營養學之貢獻的目標。我希望此網站不僅是個關於我的平台，更是一個可以分享我遇到這些故事的角落。</p>
             </section>
         );
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,12 @@ import { NavLink } from "react-router-dom";
 
 export default class Navbar extends React.Component {
     render() {
-        const className = this.props.className;
         return (
             <nav className="navbar-height" onMouseEnter={this.props.onHover} onMouseLeave={this.props.onHoverOut} onClick={this.props.onClick}>
                 <ul>
-                    <NavButton className={className} to="/journey" activeClassName="active-tab">journey</NavButton>
-                    <NavButton exact className={className} to="/"><h1>Justin Hoong</h1></NavButton>
-                    <NavButton className={className} to="/articles" activeClassName="active-tab">articles</NavButton>
+                    <li><a href="https://hoongjustin.wixsite.com/website" activeClassName="active-tab">journey</a></li>
+                    <NavButton exact to="/"><h1>Justin Hoong</h1></NavButton>
+                    <li><a href="https://medium.com/justinhoong">blog</a></li>
                 </ul>
             </nav>
         );
